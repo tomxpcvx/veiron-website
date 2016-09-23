7722a49439dc2cdcf1c68a6c358c209acc7e692a
tomxpcvx <dev@tomxpcvx.wtf>
2016-09-23 18:16:41 +0200

    Added UserSessionHash and UserIngameName GET Request-files to the API

---
+++ api/get/UserIngameName.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tompi
+ * Date: 11.09.2016
+ * Time: 18:08
+ */
+
+$email = $_POST["email"];
+
+if ($email != "") {
+    if (preg_match('#^([a-zA-Z0-9\.\_\-]+)@([a-zA-Z0-9\.\-]+\.[A-Za-z][A-Za-z]{1,4})$#', $email)) {
+
+            include_once("../../assets/website/php/db.php");
+
+            $db = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbdatabase);
+
+            if (mysqli_connect_errno()) {
+                printf("Verbindung fehlgeschlagen: %s\n", mysqli_connect_error());
+                header('Content-Type: application/json');
+                echo '[{"error":{errorId: "ERWEB05",description: "MySQL Error"}}]';
+                exit();
+            }
+
+            $query = mysqli_query($db, "SELECT * FROM veiron_users WHERE email='$email'");
+            $resultUser = mysqli_fetch_assoc($query);
+
+            if(mysqli_num_rows($query) != 0){
+                header('Content-Type: application/json');
+                if($resultUser["transactionId"] == "0"){
+                    $bought = "false";
+                } else {
+                    $bought = "true";
+                }
+                echo '[{"upid": "'.$resultUser["upid"].'","username": "'.$resultUser["username"].'"}]';
+            } else {
+                header('Content-Type: application/json');
+                echo '[{"error":{errorId: "ERWEB04",description: "This user dont exist!"}}]';
+            }
+            mysqli_close($db);
+
+    } else {
+        header('Content-Type: application/json');
+        echo '[{"error":{errorId: "ERWEB02",description: "Email not validate!"}}]';
+    }
+} else {
+    header('Content-Type: application/json');
+    echo '[{"error":{errorId: "ERWEB01",description: "Data not validate!"}}]';
+}
+
+?>
